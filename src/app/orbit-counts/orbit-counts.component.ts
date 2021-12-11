d8ff923f4311a3296ad7f32955291a194570cc5c
mpbelz <mpbelz@gmail.com>
2021-12-11 03:28:50 -0600

    try again...

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,7 +15,7 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  typeCount(typeName: string ): number {
+  countByType (typeName: string ): number {
     let numArr = [];
     for (let i = 0; i < this.satellites.length; i++){
       if (this.satellites[i].type.toLowerCase() === typeName) {
